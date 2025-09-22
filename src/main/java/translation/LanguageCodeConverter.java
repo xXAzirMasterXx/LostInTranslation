fd9b41fb0a612fe91611e3a61ca0d0e1cd8f74a5
XingyueHe102 <xingyue.he@mail.utoronto.ca>
2025-09-22 22:57:08 +0800

    task1 version1

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -18,6 +18,7 @@ public class LanguageCodeConverter {
 
     private final Map<String, String> languageCodeToLanguage = new HashMap<>();
     private final Map<String, String> languageToLanguageCode = new HashMap<>();
+    private final Object IOException;
 
     /**
      * Default constructor that loads the language codes from "language-codes.txt"
@@ -42,13 +43,19 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
-            }
+                if (line.isEmpty()) continue;
+                int lastSpace = line.lastIndexOf(' ');
+                if (lastSpace == -1) continue;
+                String language = line.substring(0, lastSpace).trim();
+                String code = line.substring(lastSpace + 1).trim();
+                languageCodeToLanguage.put(language, code);
+                languageToLanguageCode.put(language, code);
 
-        } catch (IOException | URISyntaxException ex) {
-            throw new RuntimeException(ex);
+            } catch(IOException | URISyntaxException ex){
+                throw new RuntimeException(ex);
+            }
         }
-    }
+
 
     /**
      * Return the name of the language for the given language code.
@@ -56,8 +63,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +72,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
